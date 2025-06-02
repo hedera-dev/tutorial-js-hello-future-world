bf6997c8d8b5d0badeb4d8d2381195e382857355
Michiel Mulders <michielmulders1@gmail.com>
2025-06-02 20:13:55 +0200

    Update 03-get-dependencies.sh
    Update JSON RPC from Hedera to Hiero-Ledger

--- util/03-get-dependencies.sh
+++ util/03-get-dependencies.sh
@@ -21,7 +21,7 @@ cd ${DIR}
 export RPC_RELAY_VERSION=$( curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/hashgraph/hedera-json-rpc-relay/releases?per_page=1" |
+  "https://api.github.com/repos/hiero-ledger/hiero-json-rpc-relay/releases?per_page=1" |
   jq -r ".[].tag_name" |
   cut -c2-
 )
